143e124209e4a8100ac1cf5abed52223c9b828a9
Chris Gantt <gantt.art@gmail.com>
2021-05-11 19:15:24 -1000

    Index now gets the images.json from the bucket and appends the latest file upload information to the object.

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ exports.handler = async (event, context, callback) => {
   console.log('object data', metadata);
 
   const bucket = event.Records[0].s3.bucket.name;
-  const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-  console.log('bucket & key', bucket, key);
+  // const key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
+  // console.log('bucket & key', bucket, key);
 
-  const imageJson = await s3.getObject({ Bucket: bucket, Key: 'images.json' }).promise();
-  console.log('image json', imageJson);
-  console.log('json', imageJson.Body);
-  console.log('data to string', imageJson.Body.toString('utf-8'));
-  console.log('data to json', JSON.parse(imageJson.Body.toString('utf-8')));
+  let s3object = await s3.getObject({ Bucket: bucket, Key: 'images.json' }).promise();
+  console.log('image json', s3object);
+  console.log('json', s3object.Body);
+  console.log('data to string', s3object.Body.toString('utf-8'));
+  console.log('data to json', JSON.parse(s3object.Body.toString('utf-8')));
+
+  let imagesObject = JSON.parse(s3object.Body.toString('utf-8'));
+  imagesObject.images.push(metadata);
+  console.log('imagesObject post push', imagesObject);
+
+  var base64data = new Buffer.from(JSON.stringify(imagesObject), 'binary');
+
+  s3.putObject({
+    Bucket: bucket,
+    Key: 'images.json',
+    Body: base64data,
+    ACL: 'public-read'
+  });
+  // from: https://gist.github.com/homam/8646090
+
+  console.log('bottom of index');
 
   return 'return lambda';
 }
